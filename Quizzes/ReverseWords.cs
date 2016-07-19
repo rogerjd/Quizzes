153ed47bf7de848b41daf89f59574e8e5b3729d5
roger <rjpd@netzero.com>
2016-07-19 04:00:32 -0400

    init

---
+++ Quizzes/ReverseWords.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizzes
+{
+    static class ReverseWords
+    {
+        const string inStr = "The quick brown fox, jumped over the lazy dog.";
+        static string delims = ", .";
+
+        public static void Main()
+        {
+            string word = "";
+            string res = "";
+
+            for (int i = 0; i < inStr.Length ; i++)
+            {
+                if (delims.Contains(inStr[i]))
+                {
+                    if (word != "")
+                    {
+                        res += RevWord(word);
+                        word = "";
+                    }
+                    res += inStr[i];
+                }
+                else
+                {
+                    word += inStr[i];
+                }
+            }
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+
+        private static string RevWord(string word)
+        {
+            string res = "";
+            for (int i = word.Length-1; i >= 0; i--)
+            {
+                res += word[i];
+            }
+            return res;
+        }
+    }
+}
